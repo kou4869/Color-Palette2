2abc431c9d3790b2f4d46605540670dd3e6fa859
EC2 Default User <119918517+kou4869@users.noreply.github.com>
2023-03-06 12:28:38 +0000

    会員ログイン機能実装

--- db/migrate/20230306061554_devise_create_users.rb
+++ db/migrate/20230306061554_devise_create_users.rb
@@ -8,7 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, null: false, default: ""
 
       t.string :name, null: false
-      t.text :information
+      t.text :introduction
 
       ## Recoverable
       t.string   :reset_password_token
