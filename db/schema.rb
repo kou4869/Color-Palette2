00eed9659269e10177589d955855b202a41bc7de
EC2 Default User <119918517+kou4869@users.noreply.github.com>
2023-03-07 11:32:44 +0000

    タグ更新

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2023_03_07_031707) do
 
-  create_table "adimins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_adimins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_adimins_on_reset_password_token", unique: true
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -58,20 +46,6 @@ ActiveRecord::Schema.define(version: 2023_03_07_031707) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "publics", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.text "information"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_publics_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_publics_on_reset_password_token", unique: true
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "tag_name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -82,7 +56,7 @@ ActiveRecord::Schema.define(version: 2023_03_07_031707) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.text "information"
+    t.text "introduction"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
